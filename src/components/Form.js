a7e6d2bcd200dd576b5e6df7b89f7335b153c272
Jeff Lyons <jeffreylyons@outlook.com>
2022-03-11 11:03:51 -0500

    fifth commit

---
+++ src/components/Form.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import {useFormik} from 'formik'
+
+const initialValues = {
+    firstName: '',
+    lastName: '',
+    city: '',
+    country: '',
+    employer: '',
+    title: '',
+    movieOne: '',
+    movieTwo: '',
+    movieThree: '',
+}
+
+function Form() {
+
+    const formik = useFormik({
+        initialValues,
+        
+    })
+
+
+  return (
+        <div className='form'>
+            <form className='insideForm'>
+                <h1><u>Enter User Data</u></h1>
+                <label>First Name*</label> <input 
+                type='text'
+                id='firstName'
+                name='firstName'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                <br></br>
+                <br></br>
+                <label>Last Name*</label> <input 
+                type='text'
+                id='lastName'
+                name='lastName'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                   <br></br>
+                <label>City*</label> <input 
+                type='text'
+                id='city'
+                name='city'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                   <br></br>
+                <label>Country*</label> <input 
+                type='text'
+                id='country'
+                name='country'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                   <br></br>
+                <label>Employer*</label> <input 
+                type='text'
+                id=''
+                name=''
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                   <br></br>
+                <label>Title*</label> <input 
+                type='text'
+                id='title'
+                name='title'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                   <br></br>
+                <label>3 Favorite Movies*</label> 
+                <br></br>
+                <input 
+                type='text'
+                id='movieOne'
+                name='movieOne'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                <label></label> <input 
+                type='text'
+                id='movieTwo'
+                name='movieTwo'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                   <br></br>
+                <label></label> <input 
+                type='text'
+                id='movieThree'
+                name='movieThree'
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                />
+                <br></br>
+                <br></br>
+                <button type='submit'>Submit</button>
+
+                <p>* Required Data</p>
+               
+            </form>
+        </div>
+  )
+}
+
+export default Form
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,19 +3,50 @@ import Header from '../components/Header';
 import NameCard from './NameCard';
 import data from '../data';
 import '../styles/styles.css'
+import Form from './Form';
+
+const cards = data.map(item =>{
+  return(
+    <NameCard
+      {...item}
+    />
+  )
+})
 
 function Main() {
 
-  const cards = data.map(item =>{
-    return(
-      <NameCard
-        {...item}
-      />
-    )
-  })
-  
-  const [user, setUser] = React.useState(0)
 
+  
+  const [showForm, setShowForm] = React.useState(false)
+  const [userData, setUserData] = React.useState(cards)
+  const [user, setUser] = React.useState(0)
+  
+  function formOn(){
+    setShowForm(prevShowForm => !prevShowForm)
+    console.log(showForm)
+  }
+  
+  function displayForm(){
+    if(showForm === true){
+      return(
+        <Form/>
+        )
+      }
+    }
+    
+    function deleteUser(user){
+      let id = user + 1
+      
+      const newUserData = data.filter((item) => item.id !== id);
+      
+      setUserData(newUserData)
+      console.log('clicked')
+      
+      console.log(newUserData)
+
+      
+    }
+    
   const nextUser = () => {
     if(user < 24){
       setUser(user + 1)
@@ -32,16 +63,19 @@ function Main() {
     <>
     <Header/>
     <div className='user-card-main'>
-    <section>{cards[user]}</section>
+    <section>{userData[user]}</section>
     <div className='user-buttons'>
-      <button className='user-change1' onClick={prevUser}>Previous</button>
+      <button className='user-change1' onClick={prevUser}>&#60; Previous</button>
     <div className='center-btns'>
       <button className='button-mid'>Edit</button>
-      <button className='button-mid'>Delete</button>
-      <button className='button-mid'>New</button>
+      <button className='button-mid' onClick={() => deleteUser(user)}>Delete</button>
+      <button className='button-mid' onClick={formOn}>New</button>
     </div>
-      <button className='user-change2' onClick={nextUser}>Next</button>    
+      <button className='user-change2' onClick={nextUser}>Next &#62;</button>    
     </div>
+    </div>
+    <div>
+      {displayForm(false)}
     </div>
     </>
     
